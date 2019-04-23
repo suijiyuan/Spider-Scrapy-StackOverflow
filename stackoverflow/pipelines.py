eddeb53602b5f68910b6eceff59175d74a6b3b38
suijiyuan <suijiyuan1996@gmail.com>
2019-04-23 20:12:49 +0800

    StackOverflowSpider

--- stackoverflow/pipelines.py
+++ stackoverflow/pipelines.py
@@ -2,12 +2,17 @@ import os
 
 
 class StackOverflowPipeline(object):
-    def process_item(self, item, spider):
+    def __init__(self):
         base_dir = os.getcwd()
         url = base_dir + '/datasets'
         if not os.path.exists(url):
             os.mkdir(url)
         file_path = url + '/data'
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    def process_item(self, item, spider):
+        file_path = os.getcwd() + '/datasets/data'
         with open(file_path, 'a') as file:
             file.write(str(item) + '\n')
         return item
